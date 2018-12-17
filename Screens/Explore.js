2423ccce00e05ee8960539d18e4237b6459cf708
huynn <nhat.huy.7996@gmail.com>
2018-12-17 18:12:34 +0700

    update 2, nothing much, just sync before leave office

--- Screens/Explore.js
+++ Screens/Explore.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView,TextInput,Platform,
-        StatusBar } from 'react-native';
+        StatusBar,ScrollView } from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 
@@ -19,13 +19,13 @@ class Explore extends React.Component {
      <SafeAreaView style={{flex: 1}}>
         <View style={{flex: 1}}>
             <View style={{height: this.startHeaderHeight, backgroundColor:'white',borderBottomWidth: 1,
-            borderBottomColor: 'white'}}>
+            borderBottomColor: '#dddddd'}}>
                 <View style={{flexDirection: 'row', padding: 10,
                     backgroundColor: 'white', marginHorizontal: 20,
                     shadowOffset:{width:0,height: 0},
                     shadowColor: 'black', shadowOpacity: 0.2,
                     elavation: 1,
-                    marginTop: Platform.OS == 'android' ? 30: null }}>
+                    marginTop: Platform.OS == 'android' ? 30: this.startHeaderHeight/3 }}>
                     <Icon name='ios-search' size={24} style={{marginRight: 10}} />
                     <TextInput 
                         placeholder ="Try"
@@ -35,6 +35,14 @@ class Explore extends React.Component {
                     />
                 </View>
             </View>
+            
+            <ScrollView
+            scrollEventThrottle={16}>
+                <View style={{flex: 1,backgroundColor: 'white', paddingTop: 20}}>
+                    <Text style={{fontSize: 24, fontWeight: '700', paddingHorizontal: 20}} > I miss U </Text>
+                </View>
+            </ScrollView>
+
         </View>
      </SafeAreaView>
     );
